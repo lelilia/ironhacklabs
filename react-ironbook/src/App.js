9825478c902286dd97c8dee161710b07a168c4d1
Leli <lelilia@posteo.de>
2020-08-10 06:02:20 +0200

    ironbook finished

--- react-ironbook/src/App.js
+++ react-ironbook/src/App.js
@@ -47,22 +47,60 @@ const Table = ({ users }) => {
 
 const App = () => {
   const [nameFilter, setNameFilter] = useState('')
+  const [ studentCheck, setStudentCheck ] = useState(true)
+  const [ teacherCheck, setTeacherCheck ] = useState(true)
+  const [ campus, setCampus ] = useState('all')
 
   const handleFilterChange = (event) => setNameFilter(event.target.value)
+  const handleCampusChange = (event) => setCampus(event.target.value)
 
-  const usersToShow = nameFilter
-    ? users.filter(user => 
-        user.firstName.toLowerCase().includes(nameFilter.toLowerCase()) ||
-        user.lastName.toLowerCase().includes(nameFilter.toLowerCase())
-      )
-    : users
+  let usersToShow = users
+  if (campus !== "all") {
+    usersToShow = usersToShow.filter(user => user.campus === campus)
+  }
+  if (!studentCheck) {
+    usersToShow = usersToShow.filter(user => user.role !== "student")
+  }
+  if (!teacherCheck) {
+    usersToShow = usersToShow.filter(user => user.role !== "teacher")
+  }
+  if (usersToShow && nameFilter) {
+    usersToShow = usersToShow.filter(user => 
+      user.firstName.toLowerCase().includes(nameFilter.toLowerCase()) ||
+      user.lastName.toLowerCase().includes(nameFilter.toLowerCase())
+    )
+  }
 
+  
+  
   return (
     <div>
 
       <h1>IronBook</h1>
       <div>
-        <input className = "namefilter" value={nameFilter} onChange={handleFilterChange} />
+        <input className = "namefilter"  placeholder ="search by name"value={nameFilter} onChange={handleFilterChange} />
+      </div>
+      <div>
+        <input 
+          type = "checkbox" 
+          id = "student"
+          checked = {studentCheck}
+          onChange = {() => setStudentCheck(!studentCheck)}
+        />
+        <label htmlFor ="student">Student</label>
+        <input 
+          type = "checkbox" 
+          id = "teacher" 
+          checked = {teacherCheck}
+          onChange = {() => setTeacherCheck(!teacherCheck)}
+        />
+        <label htmlFor ="teacher">Teacher</label>
+        <select value = {campus} onChange = {handleCampusChange}>
+          <option value = "all">all</option>
+          <option value = "Berlin">Berlin</option>
+          <option value = "Lisbon">Lisbon</option>
+          <option value = "Paris">Paris</option>
+        </select>
       </div>
       <Table users={usersToShow} />
     </div>
